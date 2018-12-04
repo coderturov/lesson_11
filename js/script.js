3d13d9eecdba31514be7b704b9b4e2b654c78c3d
Vladimir <vladimir_t1@mail.ru>
2018-12-05 01:54:17 +0500

    готово

--- js/script.js
+++ js/script.js
@@ -117,4 +117,55 @@ descBtn.forEach(function(item) {
     });
 });
 
+//Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+       
+
+        statusMessage.classList.add('status');
+
+        // input.addEventListener('tel', restrictToInteger); 
+ 
+        // function restrictToInteger() { 
+         
+        //     this.value = this.value.replace(/[^\d.]/g, ''); 
+         
+        // }
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader ('Content-Type', 'application/x-www-urlencoded');
+
+        let formData = new FormData(form);
+        request.send(formData);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if(request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        for(let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
+
+
 });
+
